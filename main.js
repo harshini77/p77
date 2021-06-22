c09dc03ab65e0ac732e56487f0da63403d4742ab
harshini77 <82214608+harshini77@users.noreply.github.com>
2021-06-22 16:14:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log("my first console project");
+
+var first_name = "harshini";
+var last_name = "kumar";
+var full_name = first_name.contact(last_name);
+console.log(full_name);
+ var a = 10;
+ var b = 5;
+ var add = a+b;
+  console.log(add);
+  var subtract = a-b;
+  console.log(subtract);
+  var multiply = a*b;
+  console.log(multiply);
+  var divide = a/b;
+  console.log(divide);
